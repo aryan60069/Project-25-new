5d8eac9a310cee9edc8c0fe0e791c1b053ef62f5
aryan60069 <78200051+aryan60069@users.noreply.github.com>
2021-04-12 20:54:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject, paperball;
+var world;
+
+
+function setup() {
+	createCanvas(1500, 700);
+	rectMode(CENTER);
+
+    engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	paperball = new Paper(200,500,80);
+
+	Engine.run(engine);
+  }
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+  //keyPressed();
+  groundObject.display();
+  dustbinObj.display();
+  paperball.display();
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+  Body.applyForce(paperball.body, paperball.body.position,{x:220, y:-190})
+}
+}
